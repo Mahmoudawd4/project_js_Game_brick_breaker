f07760930271a381cfabe8bab98d022a34717b9f
Adel Abdellatif <Adel.A.Abdelkader@gmail.com>
2023-01-28 02:25:24 +0200

    remove unneeded for

--- JS/logic.js
+++ JS/logic.js
@@ -158,11 +158,6 @@ document.addEventListener("keydown", function (event) {
         game.score = 0;
         game.lives = 3;
         initBricks();
-        for (let c = 0; c < brick.columnCount; c++) {
-            for (let r = 0; r < brick.rowCount; r++) {
-                bricks[c][r].status = 1;
-            }
-        }
         ballInit();
         play();
     }
